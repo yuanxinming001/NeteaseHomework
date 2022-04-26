76af4a1d41bd6efd707b8dec29da7b72ad254a9d
yuanxinming <wb.yuanxinming@mesg.corp.netease.com>
2022-04-26 20:56:53 +0800

    修改发送邮件函数

--- startmail.py
+++ startmail.py
@@ -4,32 +4,29 @@ from email.mime.text import MIMEText
 
 
 
-def sendemail():
+# 登陆邮箱
+sent = smtplib.SMTP()
+sent.connect('smtp.qq.com', 25)
+mail_name = "2431542413@qq.com"  # 发送人邮箱地址
+mail_password = "rmuhrohrwfzodifa"  # 注意：这里不是密码，而应该填写授权码！！
+sent.login(mail_name, mail_password)  # 登陆
 
-    # 登陆邮箱
-    sent = smtplib.SMTP()
-    sent.connect('smtp.qq.com', 25)
-    mail_name = "2431542413@qq.com"  # 发送人邮箱地址
-    mail_password = "rmuhrohrwfzodifa"  # 注意：这里不是密码，而应该填写授权码！！
-    sent.login(mail_name, mail_password)  # 登陆
+# 编辑邮件内容
+to = ['2431542413@qq.com']  # 收件人邮箱地址
+content = MIMEText('最爱你的人发给你的第一封邮件💗')  # 正文内容
+content['Subject'] = '漂流瓶'  # 邮件标题
+content['From'] = mail_name  # 发件人
+content['To'] = ','.join(to)  # 收件人，用逗号连接多个邮件，实现群发
 
-    # 编辑邮件内容
-    to = ['2431542413@qq.com']  # 收件人邮箱地址
-    content = MIMEText('最爱你的人发给你的第一封邮件💗')  # 正文内容
-    content['Subject'] = '漂流瓶'  # 邮件标题
-    content['From'] = mail_name  # 发件人
-    content['To'] = ','.join(to)  # 收件人，用逗号连接多个邮件，实现群发
-
-    # 发送邮件
-    try:
-        sent.sendmail(mail_name, to, content.as_string())  # 3个参数 发送人，收件人，邮件内容
-        print('邮件Success')
-        sent.close()
-    except smtplib.SMTPException:
-        print("Error：Fail")
+# 发送邮件
+try:
+    sent.sendmail(mail_name, to, content.as_string())  # 3个参数 发送人，收件人，邮件内容
+    print('邮件Success')
+    sent.close()
+except smtplib.SMTPException:
+    print("Error：Fail")
 
 
-sendemail()
 
 
 
